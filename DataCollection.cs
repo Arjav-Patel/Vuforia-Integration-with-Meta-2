4dcc726079d0f6f3fb29adb7ee9638577a618a86
Arjav-Patel <33484527+Arjav-Patel@users.noreply.github.com>
2018-07-09 15:00:23 -0400

    Add files via upload

---
+++ DataCollection.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DataCollection : MonoBehaviour {
+
+    public float pos;
+    public float increment;
+    private string path = "Assets/Resources/data.txt";
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            WriteString();
+
+            Debug.Log(pos);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            pos += increment;
+
+            Debug.Log("increment");
+            Debug.Log(pos);
+        }
+    }
+
+
+    private void WriteString()
+    {
+        StreamWriter wr = new StreamWriter(path, true);
+        wr.WriteLine(transform.position.z + " " + pos);
+        wr.Close();
+
+        Debug.Log("wrote");
+    }
+}
