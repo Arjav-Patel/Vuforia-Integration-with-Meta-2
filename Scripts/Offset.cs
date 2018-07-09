ad42c479ba69c32f85bb38b3f4c34768bbf695c6
Arjav-Patel <33484527+Arjav-Patel@users.noreply.github.com>
2018-07-09 15:02:07 -0400

    Add files via upload

---
+++ Scripts/Offset.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Offset : MonoBehaviour
+{
+
+    // Public variables are initialized in the inspector; private variables are initialized at Start
+
+    // Marker Components
+    public GameObject Marker;
+    Transform MarkerTransform;
+
+    // Image Components
+    Renderer ImageRenderer;
+
+    // Fine-tune adjustments for gameobject position
+    public float xFix;
+    public float yFix;
+    public float zFix;
+
+    private void Start()
+    {
+        // Marker Components
+        MarkerTransform = Marker.transform;
+
+        // Image Components
+        ImageRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        // Target is found
+        if (ImageRenderer.enabled)
+        {
+            // Calculate new position relative to Vuforia Camera (0, 0, 0) based on current position (which is offset)
+            float x = CalculateOffsetX(MarkerTransform.position.x);
+            float y = CalculateOffsetY(MarkerTransform.position.y);
+            float z = CalculateOffsetZ(MarkerTransform.position.z);
+
+            float angleX = MarkerTransform.eulerAngles.x;
+            float angleY = MarkerTransform.eulerAngles.y;
+            float angleZ = MarkerTransform.eulerAngles.z;
+
+            // Set new position
+            Vector3 pos = new Vector3(x, y, z);
+            transform.localPosition = pos;
+
+            // Set new rotation
+            Vector3 rot = new Vector3(angleX, angleY, angleZ);
+            transform.localEulerAngles = rot;
+        }
+    }
+
+    private float CalculateOffsetX(float MarkerPosX)
+    {
+        return MarkerPosX + xFix;
+    }
+
+    private float CalculateOffsetY(float MarkerPosY)
+    {
+        return MarkerPosY + yFix;
+    }
+
+    private float CalculateOffsetZ(float MarkerPosZ)
+    {
+        // replace these
+        float m = 0.552f;
+        float b = 0.0147f;
+
+        return m * MarkerPosZ + b + zFix;
+    }
+}
---
+++ Scripts/RGBTransform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Meta;
+using Meta.Interop;
+using Meta.Plugin;
+
+public class RGBTransform : MonoBehaviour {
+
+    public MetaCoreInterop.MetaCoordinateFrame destination;
+    public MetaCoreInterop.MetaCoordinateFrame source;
+
+    public Matrix4x4 current_transform = Matrix4x4.identity;
+    public bool continuous_update_ = false;
+
+    private bool recieved_transform_ = false;
+
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+
+    // angleX is set to -13 because that is what worked for me
+    public float angleX = 0f;
+    public float angleY = 0f;
+    public float angleZ = 0f;
+    
+    void Update()
+    {
+
+        if (!recieved_transform_ || continuous_update_)
+        {
+            // Use Meta API to find coordiations
+            recieved_transform_ = SystemApi.GetTransform(destination, source, ref current_transform);
+
+            // Meta API uses a right-hand rule, GetPosition() and GetRotation() change this to left-hand rule coordinates
+            transform.localPosition = current_transform.GetPosition();
+            transform.localRotation = current_transform.GetRotation();
+
+            // Use this to fix position offset
+            Vector3 pos = new Vector3(x, y, z);
+            pos += transform.position;
+
+            transform.position = pos;
+
+            // Use this to fix rotation offset
+            transform.Rotate(angleX, angleY, angleZ, Space.World);
+
+        }
+
+    }
+}
