9767d0b647144599b78cb774fef84ac9d79fa64a
Arjav-Patel <33484527+Arjav-Patel@users.noreply.github.com>
2018-07-16 15:40:48 -0400

    Add files via upload

---
+++ Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using Vuforia;
+
+/// <summary>
+///     A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PRIVATE_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+
+    #endregion // PRIVATE_MEMBER_VARIABLES
+
+    #region UNTIY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PRIVATE_METHODS
+    
+    // Public variables that work with Offset.cs to enable and disable mesh renderer appriopriately
+    public bool found = false;
+    public bool changedState = false;
+
+    protected virtual void OnTrackingFound()
+    {
+        found = true;
+        changedState = true;
+
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        found = false;
+        changedState = true;
+    }
+
+    #endregion // PRIVATE_METHODS
+}
